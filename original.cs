b3ef6afe6910f4a263ab59460739fc40190f6de6
zarkasim <12100876@students.koi.edu.au>
2023-12-11 16:11:06 +0500

    for testing

---
+++ original.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace SydneyCoffee
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(String.Format("The customer spending least is {0} ${1}"));
+
+        }
+    }
+}
